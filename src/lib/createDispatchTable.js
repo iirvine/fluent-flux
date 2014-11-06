359dffe70e664afaf9bfd66875404259cd8cbc34
iirvine <icirvine@gmail.com>
2014-11-06 12:17:48 -0800

    allow stores to declare a `beforeHandler` - a handler that runs before every other handler

--- src/lib/createDispatchTable.js
+++ src/lib/createDispatchTable.js
@@ -4,13 +4,6 @@ var DispatchTable = require('../DispatchTable');
 const TablePostfix = "_DispatchTable";
 
 module.exports = function createDispatchTable(handlers, displayName) {
-	if (!handlers || !handlers.length) {
-		invariant(
-			false,
-			`createDispatchTable(...): Store ${store.displayName} has not declared any handlers.`
-		);
-	}
-
 	var spec = {
 		displayName: (displayName ? displayName : "") + TablePostfix
 	}
--- src/lib/createStore.js
+++ src/lib/createStore.js
@@ -1,6 +1,14 @@
 var BaseStore = require('../Store');
 var assign = require('object-assign');
 var invariant = require('./invariant');
+var createDispatchRecord = require('./createDispatchRecord');
+
+function before(handler, fn) {
+	return function() {
+		fn.apply(this, arguments);
+		handler.apply(this, arguments);
+	}
+}
 
 module.exports = function createStore(spec) {
 	class Store extends BaseStore {
@@ -16,6 +24,13 @@ module.exports = function createStore(spec) {
 		spec.handlers,
 		`Store ${spec.displayName} has not declared any handlers.`
 	);
+
+	if (spec.beforeHandler) {
+		spec.handlers = spec.handlers.map((record) => {
+			var [action, handler] = record;
+			return createDispatchRecord(action, before(handler, spec.beforeHandler))
+		})
+	}
 
 	assign(Store.prototype, spec);
 	return new Store();
